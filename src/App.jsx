3b1e7be2b74ee310ab385d226e1b88ec80b8d585
SC3 <isc18350501@gmail.com>
2022-04-05 10:01:43 -0500

    input

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,29 @@ import {useState} from "react";
 function App() {
 
   const [cuenta, setCuenta] = useState(0);
+  const [paso, setPaso] = useState(1);
 
   const handleClick=()=>{
-    setCuenta(cuenta +1);
+    setCuenta(cuenta +paso);
   }
 
    const handleClickRes=()=>{
-    setCuenta(cuenta -1);
+     console.log(paso);
+    setCuenta(cuenta - paso);
   }
 
    const handleClickLim=()=>{
-    setCuenta(cuenta - cuenta);
+    setCuenta(0);
   }
+  const handleInputChange=(event)=>{
+if(isNaN(event.target.value)){
+  return;
+}
+  
+    setPaso(Number(event.target.value));
+    
+  };
+  
   return (
     <div className="App">
       <h3>CONTADOR</h3>
@@ -22,14 +33,24 @@ function App() {
       <hr/>
       <div style={{display:"flex", 
       justifyContent:"flex-end", 
+
       marginRight:"5px",}}>
+
+        
+        <label>Paso
+        <input id="paso" type="text"  onChange={handleInputChange} value={paso} style={{marginLeft:"5px", width:"60px"}}
+        />
+        </label>
       < button type="button" className="btn btn-primary" onClick={handleClick}
+      style={{marginLeft:"5px"}}
       >+1</button>
 
 < button type="button" className="btn btn-primary" onClick={handleClickRes}
+style={{marginLeft:"5px"}}
       >-1</button>
 
 < button type="button" className="btn btn-primary" onClick={handleClickLim}
+style={{marginLeft:"5px"}}
       >Limpiar</button>
       </div>
     </div>
